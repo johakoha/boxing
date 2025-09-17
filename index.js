4dc13f257666f91c2ccfbbc8aa0742542bd72fa9
johakoha <jakhongirturaev312@gmail.com>
2025-09-17 20:21:42 +0900

    feat: learn git commands

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const calculate = require('./calculate');
 const time = moment().format("HH:mm:ss");
 const result = calculate(10, 15); //Call
 console.log("time", time, result);
+
+/* 
+GIT COMMANDS
+  git init
+  git status
+
+  git add .
+  git status
+  git commit -m "feat/fix:message"
+
+  git branch
+  git branch master
+  
+*/
